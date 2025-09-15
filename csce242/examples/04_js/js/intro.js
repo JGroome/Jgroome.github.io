f05286791a98ef353c6984ef097a870da786d122
JGroome <JGROOME@email.sc.edu>
2025-09-14 23:58:12 -0400

    base website with a little styling

--- csce242/examples/04_js/js/intro.js
+++ csce242/examples/04_js/js/intro.js
@@ -32,8 +32,9 @@ document.getElementById("btn-clear").onclick = () => {
     pFeeling.classList.remove("happy");
 }
 
-document.getElementById("txt-emotion").onKeyup = (event) => {
-    const userInput = event.currentTarget.value
-    document.getElementById("p-emotion").innerHTML = 'You are feeling $(userInput).';
+document.getElementById("txt-emotion").onkeyup = (event) => {
+    console.log
+    const userInput = event.currentTarget.value;
+    document.getElementById("p-emotion").innerHTML = `You are feeling ${userInput}`;
     document.getElementById("img-emotion").classList.remove("hidden");
 }
